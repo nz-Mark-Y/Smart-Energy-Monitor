01bb527265ac729649ec54b730a973b6b7739b46
Mark Yep <mark_yep@hotmail.com>
2016-09-14 13:58:47 +1200

    labs

--- Labs/AnalogueLab4/group37/group37/main.c
+++ Labs/AnalogueLab4/group37/group37/main.c
@@ -11,21 +11,10 @@
 
 int main(void) {
 	uart_init();	
-	unsigned int dataInt = 6969;
-	uint8_t dataArray[4];
-	uint8_t index = 0;
-		
-	for (int i=0;i<4;i++) {
-		dataArray[i] = wololo(dataInt%10, i, 0);
-	}
-	while (1) {
-		uint8_t data = dataArray[index];	//Get the integer to send	
-		uart_transmit(data);
-		_delay_ms(2);	//Small time delay so that no apparent flicker on seven segment displays
-		index++;
-		//Select next integer to send
-		if (index == 4) {
-			index = 0;
-		}
+	uint8_t dataInt = 102;
+
+	while (1) {	
+		uart_transmit(dataInt);
+		_delay_ms(2);	
 	}
 }
