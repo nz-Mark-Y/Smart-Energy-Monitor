6eee639f117de10f59b0bf45d6ca3e3a6033a8d1
Mark Yep <mark_yep@hotmail.com>
2016-09-21 20:44:02 +1200

    added comments, rearranged

--- C/group37/group37/prototypes37.h
+++ C/group37/group37/prototypes37.h
@@ -16,8 +16,8 @@ unsigned int wololo(uint8_t input, uint8_t position, uint8_t decimal);
 float calcPower(float (*voltage)[20], float (*current)[20]);
 float linearApproximate(float higher, float lower);
 void adc_init();
-unsigned int adc_read_polling();
-unsigned int adc_read_interrupt();
+unsigned int adc_read_1();
+unsigned int adc_read_2();
 float adc_calculation(unsigned int adcValue);
 
 #endif /* PROTOTYPES37_H_ */
